0b373d593269abf959e0d3097f1a7a48108e1de3
owner <owner@owners-MacBook-Pro.local>
2022-04-10 22:31:35 +0300

    Added guide option

--- decorators.py
+++ decorators.py
@@ -3,6 +3,8 @@ from functools import wraps
 from django.conf import settings
 from django.contrib import messages
 
+from django.contrib.auth.decorators import login_required, user_passes_test
+
 import requests
 
 def check_recaptcha(view_func):
@@ -27,4 +29,15 @@ def check_recaptcha(view_func):
                 messages.error(request, 'Invalid reCAPTCHA. Please try again.')
         return view_func(request, *args, **kwargs)
     return _wrapped_view
+
+''' Check if user has permition to edit blog'''
+def group_required(*group_names):
+   """Requires user membership in at least one of the groups passed in."""
+
+   def in_groups(u):
+       if u.is_authenticated:
+           if bool(u.groups.filter(name__in=group_names)) | u.is_superuser:
+               return True
+       return False
+   return user_passes_test(in_groups)
 
