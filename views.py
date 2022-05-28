713a897f87a71b74b7d89f8e7e8ccec2f184c34e
noam.gati@gmail.com <noam.gati@gmail.com>
2022-05-28 04:35:12 +0000

    Fixed website

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import render_to_response
+#from django.shortcuts import render_to_response
 
 # Create your views here.
 from django.http import HttpResponse
